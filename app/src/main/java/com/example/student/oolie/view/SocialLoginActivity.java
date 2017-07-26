f5dcbc1e0bf93ac98f4962d1297d13b502a79910
Ravali Peddi <ravali121@gmail.com>
2017-07-26 01:47:40 -0700

    Bottom Navigation Bar

--- app/src/main/java/com/example/student/oolie/view/SocialLoginActivity.java
+++ app/src/main/java/com/example/student/oolie/view/SocialLoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.student.oolie.view;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -19,6 +21,9 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
 import com.facebook.share.widget.ShareDialog;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class SocialLoginActivity extends AppCompatActivity {
@@ -27,6 +32,7 @@ public class SocialLoginActivity extends AppCompatActivity {
     TextView fb_username;
     Button logout,login;
     private ShareDialog shareDialog;
+    String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,8 +96,32 @@ public class SocialLoginActivity extends AppCompatActivity {
 
         protected void onPostExecute(Bitmap result){
             bmImage.setImageBitmap(result);
-
+           path = saveToInternalStorage(result);
         }
 
+    }
+
+    private String saveToInternalStorage(Bitmap bitmapImage){
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath=new File(directory,"profile.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
     }
 }
