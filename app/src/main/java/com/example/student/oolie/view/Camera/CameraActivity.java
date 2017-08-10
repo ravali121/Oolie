5ba1c2b067d8c357948b3fd1af6b99a677cc44d3
Student <ravali121@gmail.com>
2017-08-10 02:56:36 -0700

    Camera-Capture image & video

--- app/src/main/java/com/example/student/oolie/view/Camera/CameraActivity.java
+++ app/src/main/java/com/example/student/oolie/view/Camera/CameraActivity.java
@@ -1,29 +1,90 @@
 package com.example.student.oolie.view.Camera;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.student.oolie.R;
 import com.example.student.oolie.view.Home.HomefeedActivity;
 import com.example.student.oolie.view.Utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+
 public class CameraActivity extends AppCompatActivity {
     private static final int ACTIVITY_NUM = 1;
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
     private Context mContext = CameraActivity.this;
+    Button imageCapture, videoCapture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
         setupBottomNavigationView();
+        imageCapture = (Button)findViewById(R.id.b_image);
+        videoCapture = (Button)findViewById(R.id.b_video);
+        imageCapture.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                captureImage();
+            }
+        });
+        videoCapture.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                captureVideo();
+            }
+        });
+
+
+
+        if (!isDeviceSupportCamera()) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Your device doesn't support camera",
+                    Toast.LENGTH_LONG).show();
+            // will close the app if the device does't have camera
+            finish();
+        }
+
+    }
+    /**
+     * Checking device has camera hardware or not
+     * */
+    private boolean isDeviceSupportCamera() {
+        if (getApplicationContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+    /*
+	 * Capturing Camera Image will lauch camera app requrest image capture
+	 */
+    private void captureImage() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        // start the image capture Intent
+        startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
     }
 
-    private void setupBottomNavigationView() {
 
+
+    private void setupBottomNavigationView() {
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.botNavView);
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
         BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
